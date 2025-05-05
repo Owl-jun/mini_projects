c8d56ee374cf2d2229818a2910dfd5f8c0bba99d
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-05 21:45:16 +0900

    1회차 작업

--- asyncProject/server/main.cpp
+++ asyncProject/server/main.cpp
@@ -41,12 +41,17 @@ private:
 int main() {
 	asio::io_context io_context;
 	Server server(io_context, 9000);
-	std::thread acceptorThread([&io_context] { io_context.run(); });
+	std::vector<std::thread> threads;
+	for (int i = 0; i < 5; ++i) {
+		threads.emplace_back(std::thread([&io_context] { io_context.run(); }));
+	}
 	
-	std::cout << "[main] : 연결스레드 일하러감" << std::endl;
+	std::cout << "[main] : 스레드들 일하러감" << std::endl;
 	
-	acceptorThread.join();
-	std::cout << "[main] : 연결스레드 퇴근함, 비상상황임" << std::endl;
-
+	for (auto& thread : threads) {
+		thread.join();
+		std::cout << "[main] : 스레드 퇴근함" << std::endl;
+	}
+	std::cout << "[main] : 모든 스레드 퇴근함" << std::endl;
 	return 0;
 }
