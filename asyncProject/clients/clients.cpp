4c258f9c42f61feacbcb12d8a21666f0bacb2cbd
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-06 18:53:21 +0900

    비동기네트워킹은어려웡

--- asyncProject/clients/clients.cpp
+++ asyncProject/clients/clients.cpp
@@ -8,6 +8,7 @@ int main()
     asio::io_context io_context;
     tcp::socket socket(io_context);
     tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"), 9000);
+    std::string msg;
 
     socket.async_connect(endpoint, [&socket](std::error_code ec) {
         if (!ec) {
@@ -20,11 +21,22 @@ int main()
 
     std::thread network([&io_context] {io_context.run(); });
 
-    std::string buffer;
     while (true) {
-        std::cin >> buffer;
-        std::cout << buffer << std::flush;
+        std::getline(std::cin, msg);
+        msg += '\n';
+
+        socket.async_send(asio::buffer(msg),
+            [&msg](std::error_code ec, std::size_t bytes_transferred) {
+                if (!ec) {
+                    std::cout << "[클라] " << bytes_transferred << " bytes 전송 완료" << std::endl;
+                }
+                else {
+                    std::cout << "[클라] 전송 에러 -> " << ec.message() << std::endl;
+                }
+            });
     }
+
+    socket.close();
     network.join();
     std::cout << "프로그램을 종료합니데이 \n";
 
--- asyncProject/server/main.cpp
+++ asyncProject/server/main.cpp
@@ -1,22 +1,72 @@
 #include <iostream>
 #include <asio.hpp>
 #include <thread>
+#include <memory>
 using asio::ip::tcp;
 
-class Session {
+class Session : public std::enable_shared_from_this<Session> {
 private:
-
+	std::shared_ptr<tcp::socket> socket;
+	std::string read_msg;
 public:
+	Session(std::shared_ptr<tcp::socket> _socket) 
+		: socket(_socket)
+	{ 
+	}
 
+	void start()
+	{
+		std::cout << "[Session::start()] : 세션 시작됨" << std::endl;
+		do_read();
+	}
+
+private:
+	void do_read()
+	{
+		auto self(shared_from_this());
+		asio::async_read_until(*socket, asio::dynamic_buffer(read_msg), "\n", [this, self](std::error_code ec, std::size_t length) {
+			if (!ec) 
+			{
+				std::string msg = read_msg.substr(0, length - 1);
+				read_msg.erase(0, length);
+				std::cout << "[Session::do_read] : 클라이언트 -> " << msg << std::endl;
+
+				do_read();
+			}
+			else
+			{
+				std::cout << "[Session::do_read] : 에러 발생 -> " << ec.message() << std::endl;
+			}
+			});
+	}
+
+	/*void do_write(const std::string& msg)
+	{
+		auto self(shared_from_this());
+		asio::async_write(*socket, asio::buffer(msg),
+			[this, self](std::error_code ec, std::size_t)
+			{
+				if (!ec)
+				{
+					do_write();
+				}
+				else
+				{
+					std::cout << "[Session::do_write()] 에러 발생 -> " << ec.message() << std::endl;
+				}
+			});
+	}*/
 };
 
 
 class Server {
 private:
 	tcp::acceptor acceptor;
+	asio::io_context& io_context;
 public:
 	Server(asio::io_context& io_context, short port)
 		: acceptor(io_context, tcp::endpoint(tcp::v4(), port))
+		, io_context(io_context)
 	{
 		std::cout << "[Server::Server()] : 연결 대기중" << std::endl;
 		start_accept();
@@ -24,11 +74,12 @@ public:
 
 private:
 	void start_accept() {
-		auto socket = std::make_shared<tcp::socket>(acceptor.get_executor());
+		auto socket = std::make_shared<tcp::socket>(io_context);
 		std::cout << "[Server::start_accept] : 클라이언트 연결 수신 시작" << std::endl;
 		acceptor.async_accept(*socket, [this, socket](std::error_code ec) {
 			if (!ec) {
 				std::cout << "[Server::async_accept] : 클라이언트 연결 완료" << std::endl;
+				std::make_shared<Session>(socket)->start();
 			}
 			else {
 				std::cout << "[Server::async_accept] : 에러 -> " << ec.message() << std::endl;
@@ -39,13 +90,17 @@ private:
 };
 
 int main() {
+
 	asio::io_context io_context;
 	Server server(io_context, 9000);
+
+
 	std::vector<std::thread> threads;
 	for (int i = 0; i < 5; ++i) {
 		threads.emplace_back(std::thread([&io_context] { io_context.run(); }));
 	}
 	
+
 	std::cout << "[main] : 스레드들 일하러감" << std::endl;
 	
 	for (auto& thread : threads) {
