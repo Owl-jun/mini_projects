3d7888d09dbb52fe9a42fa23028e652c9a7cab91
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-07 21:07:45 +0900

    3회차업로드

--- asyncProject/clients/clients.cpp
+++ asyncProject/clients/clients.cpp
@@ -1,61 +1,56 @@
 ﻿#include <iostream>
 #include <asio.hpp>
-#include <thread>
-using asio::ip::tcp;
 
-std::string read_msg;
-void do_read(tcp::socket& _socket) {
-    asio::async_read_until(_socket, asio::dynamic_buffer(read_msg), "\n",
-        [&](std::error_code ec, std::size_t length) {
-            if (!ec) {
-                std::string rmsg = read_msg.substr(0, length - 1);
-                read_msg.erase(0, length);
-                std::cout << "수신 : " << rmsg << std::endl;
-                do_read(_socket); // 다시 등록
-            }
-            else {
-                std::cout << "읽기 에러 : " << ec.message() << std::endl;
-            }
-        });
-}
+using asio::ip::tcp;
 
 int main()
 {
-    asio::io_context io_context;
-    tcp::socket socket(io_context);
-    tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"), 9000);
-    std::string msg;
+    try
+    {
+        asio::io_context io_context;
 
-    socket.async_connect(endpoint, [&socket](std::error_code ec) {
-        if (!ec) {
-            std::cout << "서버접속 성공!" << std::endl;
+        // 서버 IP와 포트 (원하는대로 수정)
+        std::string server_ip = "127.0.0.1";
+        int server_port = 9000;
+
+        tcp::resolver resolver(io_context);
+        auto endpoints = resolver.resolve(server_ip, std::to_string(server_port));
+
+        tcp::socket socket(io_context);
+        asio::connect(socket, endpoints);
+
+        std::cout << "서버 연결됨.\n";
+
+        std::thread([&]() {
+            while (true) {
+                asio::streambuf buf;
+                asio::read_until(socket, buf, "\n");
+
+                std::istream is(&buf);
+                std::string received_msg;
+                std::getline(is, received_msg);
+
+                std::cout << "수신: " << received_msg << "\n";
+            }
+            }).detach();
+        std::string ID;
+        std::cout << "ID > " << std::flush;
+        std::cin >> ID;
+        while (true)
+        {
+            std::string msg;
+            std::cout << std::flush;
+            std::cout << "> ";
+            std::getline(std::cin, msg);
+
+            msg = "CHAT " + ID + " " + msg + "\n";
+            asio::write(socket, asio::buffer(msg));
         }
-        else {
-            std::cout << "에라에라 : " << ec.message() << std::endl;
-        }
-    });
-
-    std::thread network([&io_context] {io_context.run(); });
-
-    while (true) {
-        std::getline(std::cin, msg);
-        msg += '\n';
-
-        socket.async_send(asio::buffer(msg),
-            [&msg](std::error_code ec, std::size_t bytes_transferred) {
-                if (!ec) {
-                    std::cout << "[클라] " << bytes_transferred << " bytes 전송 완료" << std::endl;
-                }
-                else {
-                    std::cout << "[클라] 전송 에러 -> " << ec.message() << std::endl;
-                }
-            });
-        do_read(socket);
     }
-
-    socket.close();
-    network.join();
-    std::cout << "프로그램을 종료합니데이 \n";
+    catch (std::exception& e)
+    {
+        std::cerr << "에러: " << e.what() << "\n";
+    }
 
     return 0;
 }
